3658f354e201e090d6c5339fa4212403a15a27d6
Fco-Ivan <alvarezfcoivan@gmail.com>
2023-06-09 06:33:32 -0600

    update
    commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
+
+def mostrar_correo(request):
+    correo = "alu.18131214@correo.itlalaguna.edu.mx"
+    mensaje = f"La confirmacion del pedido se enviara a: {correo}"
+    return render(request, 'mostrar_correo.html', {'mensaje': mensaje})
