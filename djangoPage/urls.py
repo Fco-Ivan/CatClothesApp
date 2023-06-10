e288e4072b90d18a58e82cfe595143a489b0b83f
RobertoDeSantiagoDevelop <robertosantiagonajera@gmail.com>
2023-06-10 13:57:31 -0600

    another fix

--- djangoPage/urls.py
+++ djangoPage/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
