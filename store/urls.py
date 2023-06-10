419931207e960df1c467d1d4d13379a4ea122901
RobertoDeSantiagoDevelop <robertosantiagonajera@gmail.com>
2023-06-10 14:42:15 -0600

    other fix

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.store, name='store'),
@@ -8,4 +8,4 @@ urlpatterns = [
     path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
 
-#urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
